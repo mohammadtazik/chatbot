fa3f6a7271e15238e26752180283168e7a6beebd
Mohammad Tazik <tazikmohammad@gmail.com>
2025-05-25 12:40:30 +0330

    use gunicorn for production

--- chat/urls.py
+++ chat/urls.py
@@ -73,4 +73,3 @@ urlpatterns = [
     ),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
